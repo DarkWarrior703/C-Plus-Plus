556c6e05d4cba960d31d4c56c49a39fdf1699339
DarkWarrior703 <56077342+DarkWarrior703@users.noreply.github.com>
2020-02-03 13:43:17 +0200

    Update greatest_common_divisor.cpp
    Greatest common divisor algorithm is speedier with remainder operation.

--- math/greatest_common_divisor.cpp
+++ math/greatest_common_divisor.cpp
@@ -3,20 +3,9 @@
 
 // Recursive function to return gcd of a and b
 int gcd(int a, int b) {
-    // Everything divides 0
-    if (a == 0)
-       return b;
-    if (b == 0)
-       return a;
-
-    // base case
-    if (a == b)
+    if(b == 0)
         return a;
-
-    // a is greater
-    if (a > b)
-        return gcd(a-b, b);
-    return gcd(a, b-a);
+    return gcd(b, a % b);
 }
 
 // Driver program to test above function
